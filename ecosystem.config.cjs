2ef5e3a014d3d25f1a6b6d31daefd8e14f76450c
viandwi24 <viandwi24@pm.me>
2024-01-20 11:02:32 +0700

    feat: add ecosystem.config.cjs for api

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: "botfeed.viandwi24.com-api",
       script: "bun",
       args: "dev run -sp 3006",
+      cwd: "./packages/engine",
       env: {
         "NODE_ENV": "production"
       }
@@ -12,6 +13,7 @@ module.exports = {
       name: "botfeed.viandwi24.com-web",
       script: "bun",
       args: "preview",
+      cwd: "./packages/web-ui",
       env: {
         "PORT": 3007,
         "NODE_ENV": "production"
